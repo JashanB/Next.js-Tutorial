d3d98c16742e22882fefe6404034f4491f7dab05
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-22 15:48:12 -0400

    adding create note function

--- app/notes/CreateNote.tsx
+++ app/notes/CreateNote.tsx
@@ -6,8 +6,21 @@ export default function CreateNote() {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
 
+    const create = async () => {
+        await fetch('http://127.0.0.1:8090/api/collections/notes/records', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title,
+                content,
+            }),
+        });
+    }
+
     return (
-        <form>
+        <form onSubmit={create}>
             <h3>Create a new Note</h3>
             <input
                 type="text"
@@ -15,7 +28,7 @@ export default function CreateNote() {
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
             />
-            <textarea 
+            <textarea
                 placeholder='Content'
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
