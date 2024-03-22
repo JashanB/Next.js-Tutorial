dbf919dfc8dd1c23412e55ac050d0f8a262b7888
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-22 14:22:22 -0400

    adjusted id directory name to act as route

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -8,10 +8,15 @@ async function getNote(noteId: string) {
 
 export default async function NotePage({params}: any) {
     const note = await getNote(params.id);
-
+    console.log(note)
     return (
         <div>
             <h1>notes/{note.id}</h1>
+            <div>
+                <h3>{note.title}</h3>
+                <h5>{note.content}</h5>
+                <p>{note.created}</p>
+            </div>
         </div>
     )
 }
