53aa786122a1cd7f384970476773071fff6bd39d
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-22 13:25:55 -0400

    adding single note functions

--- app/notes/id/page.tsx
+++ app/notes/id/page.tsx
@@ -2,5 +2,16 @@ async function getNote(noteId: string) {
     const res = await fetch(`http://127.0.0.1:8090/api/collections/notes/records/${noteId}`, {next: {revalidate: 10}});
     const data = await res.json();
     console.log(data)
-    return data?.items as any[];
+    return data;
+}
+
+
+export default async function NotePage({params}: any) {
+    const note = await getNote(params.id);
+
+    return (
+        <div>
+            <h1>notes/{note.id}</h1>
+        </div>
+    )
 }
