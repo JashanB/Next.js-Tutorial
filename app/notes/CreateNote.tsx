8493b9bfdb07e1fda800fa682551c5a5394504f7
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-22 15:44:40 -0400

    adding states and form fields

--- app/notes/CreateNote.tsx
+++ app/notes/CreateNote.tsx
@@ -1,9 +1,20 @@
 'use server'
 
-export default function CreateNote () {
+import { useState } from 'react';
+
+export default function CreateNote() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
     return (
         <form>
-            
+            <h3>Create a new Note</h3>
+            <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
         </form>
     )
 }
